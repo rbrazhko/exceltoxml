9411d51a7d1fbdc80262b944efae67f95d58728f
Roman Brazhko <roman.brazhko@gmail.com>
2018-05-14 15:34:11 +0300

    Added new categories

--- toXml/XmlTemplates.php
+++ toXml/XmlTemplates.php
@@ -21,7 +21,7 @@ class XmlTemplates
                 <category id="3">Компьютеры и ноутбуки</category>
                 <category id="4">Медиаплееры</category>
                 <category id="5">Автотовары</category>
-                <category id="6">Красота и здоровье</category>
+                <category id="6">Красота, здоровье, уход</category>
                 <category id="7">Товары для дома</category>
                 <category id="8">Электроника</category>
                 <category id="9">Игрушки для детей</category>
@@ -29,6 +29,18 @@ class XmlTemplates
                 <category id="11">Всё для ремонта, инструменты</category>
                 <category id="12">TV Shop товары</category>
                 <category id="13">Кошельки и клатчи</category>
+                <category id="14">Гаджеты и подарки</category>
+                <category id="15">Метеостанции и барометры</category>
+                <category id="16">Уход и хранение одежды и обуви</category>
+                <category id="17">Садовый инвентарь</category>
+                <category id="18">Электрические проточные водонагреватели</category>
+                <category id="19">Шведские стенки и турники</category>
+                <category id="20">Шланги</category>
+                <category id="21">Ночники</category>
+                <category id="22">Аксессуары для дверей</category>
+                <category id="23">Кофеварки</category>
+                <category id="24">Рюкзаки</category>
+                <category id="25">Уличное освещение</category>
             </categories>
             <offers>
                 [[OFFERS]]
